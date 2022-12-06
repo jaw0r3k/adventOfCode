8eaec86f7b1127168060e95549906bc2c3a89459
Jaworek <jaw0r3k.g@gmail.com>
2022-12-06 17:47:13 +0100

    Update and rename day_1/part_1.rb to day_6/part_1.rb

--- day_6/part_1.rb
+++ day_6/part_1.rb
@@ -1,11 +1,11 @@
 file = File.open("data.txt")
 file_data = file.read().split('')
 file_data.each_with_index{|ch, index| 
-    if index > 3
+    if index > 4 # start form 5th index
         start_index = index - 4 
         end_index= index - 1 
-        sliced = file_data.slice(start_index..end_index)
-        if sliced.uniq.length == sliced.length
+        sliced = file_data.slice(start_index..end_index) # slice it
+        if sliced.uniq.length == sliced.length # check if in slicesd array arent any duplicates
             print("Character: " + ch, "Index: " + index.to_s)
             exit
         end
